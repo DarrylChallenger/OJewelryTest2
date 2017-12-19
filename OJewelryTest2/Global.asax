6a848b98fc1121fa08c8947b373fb2de85ed2a8f
Darryl.Challenger <Darryl.Challenger@gmail.com>
2017-12-19 10:59:47 -0500

    Add project files.

---
+++ OJewelryTest2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OJewelryTest2.MvcApplication" Language="C#" %>
---
+++ OJewelryTest2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OJewelryTest2.Startup))]
+namespace OJewelryTest2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
